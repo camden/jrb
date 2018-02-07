b98df76658def28caa3bccb1a3ef5e0b45c29fa1
julespoodles <ruizborys.j@husky.neu.edu>
2018-02-07 16:41:48 -0500

    Added images to portfolio

--- main.js
+++ main.js
@@ -1,3 +1,21 @@
 function onButtonPress() {
   alert("Thank you for visiting! This page is under construction so come back soon.");
 }
+
+var slideIndex = 1;
+showDivs(slideIndex);
+
+function plusDivs(n) {
+    showDivs(slideIndex += n);
+}
+
+function showDivs(n) {
+    var i;
+    var x = document.getElementsByClassName("scholarjetSlideshow");
+    if (n > x.length) {slideIndex = 1} 
+    if (n < 1) {slideIndex = x.length} ;
+    for (i = 0; i < x.length; i++) {
+        x[i].style.display = "none"; 
+    }
+    x[slideIndex-1].style.display = "block"; 
+}
